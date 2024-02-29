585c7e8dfde51c8b53b73e2dfaeeff8e70b76529
John Jairo Montoya <jmontoya@celerik.com>
2024-02-29 16:15:03 -0500

    Se agregan listado  de películas

--- movie/models.py
+++ movie/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Movie (models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
+    description = models.CharField(max_length=250, null= True)
     images = models.ImageField(upload_to = 'movie/images/')
     url = models.URLField(blank=True)
+    genre = models.CharField(blank=True, max_length=250)
+    year = models.IntegerField(blank=True, null= True)
--- news/views.py
+++ news/views.py
@@ -4,4 +4,4 @@ from . models import News
 # Create your views here.
 def news(request):
     newss = News.objects.all().order_by('-date')
-    return render (request, 'news.html', {'newss':news})
+    return render (request, 'news.html', {'newss':newss})
